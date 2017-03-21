ff2495635cf6debbe3ffc9c99255bdf3e76afc16
DevangThakkar <DevangThakkar@users.noreply.github.com>
2017-03-21 20:37:17 +0530

    Script to run RangerScript.R for all 10 folds
    Script to run RangerScript.R for all 10 folds generated previously

---
+++ code/loop_r.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+prefix="outputs_rf/fold"
+
+for i in {1..10};
+do
+    Rscript --max-ppsize=500000 RangerScript.R $prefix"$i/file.csv" $prefix"$i/x_te.csv" $prefix"$i/impout.csv" $prefix"$i/predout.csv"
+done
